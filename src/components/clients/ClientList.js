d0307f59e5dcc49aaa9a92ebaa73dd308d543bcf
Josh Wasserman <github email address>
2018-02-11 18:04:38 -0500

    Added a loading option for clientList. Avoid async issue.

--- src/components/clients/ClientList.js
+++ src/components/clients/ClientList.js
@@ -5,15 +5,24 @@ import ClientCard from "./ClientCard";
 
 const ClientList = props => {
   console.log("Client List", props);
-  const clientCards = props.clients.map((client, index) => {
-    return <ClientCard key={index} client={client} />;
-  });
-  return (
-    <div>
-      <h1>Your Clients</h1>
-      {clientCards}
-    </div>
-  );
+  if (!!props.clients) {
+    const clientCards = props.clients.map((client, index) => {
+      return <ClientCard key={index} client={client} />;
+    });
+
+    return (
+      <div>
+        <h1>Your Clients</h1>
+        {clientCards}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = state => ({

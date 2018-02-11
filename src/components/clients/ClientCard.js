aa6a3a3c1bcac1e1a2fc2cb4de7471f201f36957
Josh Wasserman <github email address>
2018-02-11 18:15:32 -0500

    Added preliminary client workout information.

--- src/components/clients/ClientCard.js
+++ src/components/clients/ClientCard.js
@@ -1,7 +1,28 @@
 import React from "react";
 
-const ClientCard = () => {
-  return <div>Client Card</div>;
+const ClientCard = ({ client }) => {
+  const name = `${client.first_name} ${client.last_name}`;
+
+  const workouts = client.workouts.map((workout, index) => {
+    const exercises = workout.exercises.map((e, index) => {
+      return <div key={index}>{e.name}</div>;
+    });
+    return (
+      <div key={index}>
+        <h5>
+          {workout.title} - {workout.time_of_workout}
+        </h5>
+        {exercises}
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h2> {name}</h2>
+      <h4>Recent Workouts</h4>
+      {workouts}
+    </div>
+  );
 };
 
 export default ClientCard;
